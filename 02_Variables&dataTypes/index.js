2ef3dd2bda228b52ffaad33e2ce81782645739a1
Raj Shekhar Patel <patelrajshekhar6@gmail.com>
2026-03-02 00:16:57 +0530

    Add files via upload

---
+++ 02_Variables&dataTypes/index.js
@@ -0,0 +1,55 @@
+var age = 45;
+
+if(true){
+    console.log(age);
+}
+
+function solve(){
+     var age = 45;
+     console.log(age);
+ }
+ solve()
+
+ // var is a type of variables that can be redefine mtlb ye duabra se decalre kiya ja sakta hai.
+
+
+{
+    let a = 25;
+    console.log(a);
+}
+
+let a = 10;
+a = "raj";
+a = true;
+a = null;
+// let is a type of variables that are blocked scope it means ki jaise koi brackets lage hai to uske bahar ki likhi hui cheeze nahi mani jayengi jo bhi brackets ander cheeze likhi hui hongi wahi acceptable hongi.
+
+const f = 54;
+console.log(f);
+
+// const is one time decalrable variable means it can not be redefine.
+
+
+//Data Type
+//data type is a type of data that shows which types of data is given.
+//there are two types of data types :
+//Primitive Datatype and Non-Primitive Datatype
+
+// Primitive Datatype are Numbers, String, Boolean, Null, Undefined, BigInt and Symbols.
+
+let score = 264; //(Number)
+
+score = "Raj" //(String)
+
+score = true; //(Boolean)
+
+let marks; // (undefined)
+
+score = null; //(null)
+
+score = 415645545455454545551524163154844568445645644646485464654545465464654645646546545645465456 //(BigInt)
+
+score = false;
+
+console.log(typeof score);
+//console.log(score);
