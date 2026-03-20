1fd2956d73025b41bed2ed9665bd2982e838f286
Raj Shekhar Patel <patelrajshekhar6@gmail.com>
2026-03-21 00:56:45 +0530

    Object Munipulating

---
+++ 13_ManipulationinHTML/index.js
@@ -0,0 +1,10 @@
+//document.getElementById("fpara");
+
+//document.getElementById("fheading");
+
+//document.getElementsByClassName("textMatter");
+
+//document.getElementsByTagName("p");
+
+
+// In this video we know about how to insert/access/update/delete any element from the code 
